69ccc17fe91403c7538051f1d6ddc423ca7155a0
bug_creator <wenrui.liu@ucdconnect.ie>
2022-03-12 18:51:09 +0800

    product api

--- app/utils/product.py
+++ app/utils/product.py
@@ -5,7 +5,26 @@ if __name__ == '__main__':
     leancloud.init("pPObpvTV7pQB9poQHO1NJoMP-MdYXbMMI", "pShwYQQ4JVfSStc56MvkHNrr")
 
 
-def getProductFromCategory(category_id:string,skip=0,limit=10):
+def getProductById(product_id: string):
+    """
+    use product_id to get product
+    return:  a product av object
+    example operation: get the product's information
+    product.get('title').get('english') 获取产品英文名
+    product.get('title').get('chinese') 获取产品中文名
+    product.get('description').get('english') 获取产品英文描述
+    product.get('description').get('chinese') 获取产品中文描述
+    product.get('price').get('dollar') 获取美元价格
+    product.get('price').get('CNY') 获取人民币价格
+    product.get('cover').url 获取封面图片链接
+    """
+    Product = leancloud.Object.extend('Product')
+    query = Product.query
+    product = query.get(product_id)
+    return product
+
+
+def getProductByCategory(category_id: string, skip=0, limit=10):
     """
     use category_id to get products
     return:  a list of product av object
@@ -36,5 +55,18 @@ def getProductFromCategory(category_id:string,skip=0,limit=10):
     return lst
 
 
-
-
+def getAllCategory(skip=0, limit=50):
+    """
+    get all category
+    return: a list of categories
+    example operation: get the first category information
+    result[0].get('title').get('english') 获取类别英文名
+    result[0].get('title').get('chinese') 获取类别中文名
+    result[0].get('cover').url 获取封面图片链接
+    """
+    query = leancloud.Query('ProductCategory')
+    query.limit(limit)
+    query.skip(skip)
+    query.include('title')
+    result = query.find()
+    return result
