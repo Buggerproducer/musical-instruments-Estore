9926bb9e30b323b584c24cd87b3332c139db8d3c
Maximilian <junyi.lv@ucdconnect.ie>
2022-03-17 09:38:00 +0800

    test order complete

--- app/static/script/order.js
+++ app/static/script/order.js
@@ -5,19 +5,18 @@ function MakeOrder(obj){
     let id = obj.getAttribute("id");
     const query = new AV.Query('Product');
     query.get(id).then((product) => {
-  // order 就是 objectId 为 582570f38ac247004f39c24b 的 order 实例
-  const title     = product.get('title').getObjectId();
-  const price     = product.get('price').getObjectId();
+  const titleid     = product.get('title').getObjectId();
+  const priceid     = product.get('price').getObjectId();
   const currentUser = AV.User.current();
+  const title     = product.get('title');
+  const price     = product.get('price');
+  console.log(title);
   let status = "delivering";
 order.set('price',price);
 order.set('status',status);
 order.set('title',title);
-order.set('user',currentUser.getObjectId());
-  //const price  = order.get('priority');
-//console.log(currentUser.getObjectId());
+order.set('user',currentUser);
     order.save().then((order) => {
-  // 成功保存之后，执行其他逻辑
   console.log('保存成功。objectId：'+order.getObjectId());
 }, (error) => {
       console.log("error");
@@ -25,9 +24,6 @@ order.set('user',currentUser.getObjectId());
 });
 });
 
-
-
-// 你还可以直接使用 AV.Object 的构造器
 
 
   }
