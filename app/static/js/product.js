458b19f573b37bddcbd9944d113580593856460c
Maximilian <junyi.lv@ucdconnect.ie>
2022-03-19 17:41:45 +0800

    create fuwenben product

--- app/static/js/product.js
+++ app/static/js/product.js
@@ -1,4 +1,28 @@
 
-function createProduct(title,detail,descript) {
+function createProduct() {
+    let pagecontent = editor.txt.html();
+    if(pagecontent!==''){
+        document.getElementById('right').innerHTML=pagecontent;
+          const HTMLs = AV.Object.extend('HTMLs');
+    const html  = new HTMLs();
+    html.set('englishHTML',pagecontent.toString());
+    console.log(pagecontent);
+
+   html.save().then((html) => {
+  console.log('保存成功。objectId：'+html.getObjectId());
+}, (error) => {
+      console.log("error");
+  // 异常处理
+});
+    }
+    else{
+        console.log("enter your page");
+    }
+
+
+
+}
+
+function updateProduct(title){
 
 }
