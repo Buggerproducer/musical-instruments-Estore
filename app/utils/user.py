34507ebca4eca52fa847a191cd967877e1556920
bug_creator <wenrui.liu@ucdconnect.ie>
2022-04-07 10:36:32 +0800

    collection

--- app/utils/user.py
+++ app/utils/user.py
@@ -17,5 +17,20 @@ def getOrderByUser(user_id, limit=10, skip=0):
     return result
 
 
-def getCollectionByUser():
-    pass
+def getCollectionByUser(user_id,limit=10,skip=0):
+    User = leancloud.Object.extend('_User')
+    user = User.create_without_data(user_id)
+    query = leancloud.Query('CollectionMap')
+    query.equal_to('user', user)
+    query.include('product')
+    query.include('product.title')
+    query.include('product.description')
+    query.include('product.price')
+    query.limit(limit)
+    query.skip(skip)
+    result = query.find()
+    lst = []
+    for i in result:
+        lst += [i.get('product')]
+    return lst
+
