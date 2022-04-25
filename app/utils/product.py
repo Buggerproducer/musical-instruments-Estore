d8f54fcb27ae39fb172bf449adcaa9157938eaa2
bug_creator <wenrui.liu@ucdconnect.ie>
2022-04-25 13:36:45 +0800

    update api

--- app/utils/product.py
+++ app/utils/product.py
@@ -73,6 +73,34 @@ def getProductByCategory(category_id: string, skip=0, limit=10):
     return lst
 
 
+def getProductByVisitCount( skip=0, limit=10):
+    """
+    use category_id to get products
+    return:  a list of product av object
+
+    example operation: get the first product's information
+    result[0].get('title').get('english') 获取产品英文名
+    result[0].get('title').get('chinese') 获取产品中文名
+    result[0].get('description').get('english') 获取产品英文描述
+    result[0].get('description').get('chinese') 获取产品中文描述
+    result[0].get('price').get('dollar') 获取美元价格
+    result[0].get('price').get('CNY') 获取人民币价格
+    result[0].get('cover').url 获取封面图片链接
+    """
+    # Category = leancloud.Object.extend('ProductCategory')
+    # category = Category.create_without_data(category_id)
+    query = leancloud.Query('Product')
+    # query.equal_to('category', category)
+    query.descending('visit_count')
+    query.include('title')
+    query.include('description')
+    query.include('price')
+    query.limit(limit)
+    query.skip(skip)
+    result = query.find()
+    return result
+
+
 def getAllCategory(skip=0, limit=50):
     """
     get all category
