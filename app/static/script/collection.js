f42e73944a3636a1d5cb9de8793801f93c3fc0e1
bug_creator <wenrui.liu@ucdconnect.ie>
2022-05-06 20:15:45 +0800

    cloud

--- app/static/script/collection.js
+++ app/static/script/collection.js
@@ -1,32 +1,47 @@
 let collection_id;
 
-function setCollect(){
-    var idArray = window.location.href.split("/");
+async function setCollect(){
+    // var idArray = window.location.href.split("/");
+    // var id = idArray[idArray.length-1];
+    // const Collection = AV.Object.extend('CollectionMap');
+    //     const collection = new Collection();
+    //     query.get(id).then((product) => {
+    //             const titleid     = product.get('title').getObjectId();
+    //             const priceid     = product.get('price').getObjectId();
+    //             const currentUser = AV.User.current();
+    //             const title     = product.get('title');
+    //             const price     = product.get('price');
+    //         collection.set('user',currentUser);
+    //         collection.set('product',product);
+    //         collection.set('status',true);
+    //         collection.save().then((collection) => {
+    //             collection_id = collection.id;
+    //             $("#collection").attr("onclick", "cancelCollect()");
+    //             document.getElementById('collection').innerText = "cancel collect";
+    //         }, (error) => {
+    //             console.log("error");
+    //             // 异常处理
+    //         });
+    // });
+        var idArray = window.location.href.split("/");
     var id = idArray[idArray.length-1];
-    const Collection = AV.Object.extend('CollectionMap');
-        const collection = new Collection();
-        query.get(id).then((product) => {
-                const titleid     = product.get('title').getObjectId();
-                const priceid     = product.get('price').getObjectId();
-                const currentUser = AV.User.current();
-                const title     = product.get('title');
-                const price     = product.get('price');
-            collection.set('user',currentUser);
-            collection.set('product',product);
-            collection.set('status',true);
-            collection.save().then((collection) => {
-                collection_id = collection.id;
-                $("#collection").attr("onclick", "cancelCollect()");
-                document.getElementById('collection').innerText = "cancel collect";
-            }, (error) => {
-                console.log("error");
-                // 异常处理
-            });
+    AV.Cloud.run('makeCollection', {'product_id':id}).then(function (data) {
+      // 处理结果
+        console.log(data)
+        if(data){
+            $("#collection").attr("onclick", "cancelCollect()");
+            document.getElementById('collection').innerText = "cancel collect";
+        }else {
+                    document.getElementById('collection').innerText = "Add to your wish list"
+        $("#collection").attr("onclick", "setCollect()");
+        }
+    }, function (err) {
+      // 处理报错
     });
 }
 
-function cancelCollect(){
-    var idArray = window.location.href.split("/");
+async function cancelCollect(){
+/*    var idArray = window.location.href.split("/");
     var id = idArray[idArray.length-1];
     const Collection = AV.Object.extend('CollectionMap');
     console.log(collection_id);
@@ -38,5 +53,20 @@ function cancelCollect(){
     }, (error) => {
         console.log("error");
         // 异常处理
+    });*/
+            var idArray = window.location.href.split("/");
+    var id = idArray[idArray.length-1];
+        AV.Cloud.run('makeCollection', {'product_id':id}).then(function (data) {
+      // 处理结果
+            console.log(data)
+        if(data){
+            $("#collection").attr("onclick", "cancelCollect()");
+            document.getElementById('collection').innerText = "cancel collect";
+        }else {
+                    document.getElementById('collection').innerText = "Add to your wish list"
+        $("#collection").attr("onclick", "setCollect()");
+        }
+    }, function (err) {
+      // 处理报错
     });
 }
