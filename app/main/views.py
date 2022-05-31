6fa6026bd1e13cb1c5a7c4d9d7d4c086b10522fa
Shi Huilin <huilin.shi@ucdconnect.ie>
2022-05-31 13:12:18 +0000

    Replace views.py

--- app/main/views.py
+++ app/main/views.py
@@ -154,14 +154,14 @@ def kinds_edit():
 
 
 # 商品具体信息页面
-@main.route('/productInfo/<kind_id>-<product_id>')
-def productInfo(kind_id, product_id):
-    products = product.getProductByCategory(kind_id)
-    if len(products) > 4:
-        # 随机取4个商品作为最底下的推荐商品
-        products = random.sample(products, 4)
+@main.route('/productInfo/<product_id>')
+def productInfo(product_id):
+    # products = product.getProductByCategory(kind_id)
+    # if len(products) > 4:
+    #     # 随机取4个商品作为最底下的推荐商品
+    #     products = random.sample(products, 4)
     commodity = product.getProductById(product_id, record=True)
-    return render_template("piano_en.html", kind_id=kind_id, products=products, commodity=commodity, async_mode=socketio.async_mode)
+    return render_template("piano_en.html", commodity=commodity, async_mode=socketio.async_mode)
 
 
 # 后台页面index
