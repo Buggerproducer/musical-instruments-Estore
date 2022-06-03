b46577439b35e83954da13ea6d0b580f6ccf24aa
Shenyuan Jin <2721712688@qq.com>
2022-06-04 01:45:08 +0800

    bug fixed

--- app/static/script/backend.js
+++ app/static/script/backend.js
@@ -25,7 +25,7 @@ qp.find().then((products)=>{
             query.include('category')
             query.find().then((categorymap) => {
             const category = categorymap[0].get('category')
-            $('#mostVisit').append('<tr><td>'+title+'<a href="/productInfo/'+ category.id+'-'+products[a].id+'" class="ms-1" aria-label="Open website">' +
+            $('#mostVisit').append('<tr><td>'+title+'<a href="/productInfo/'+ products[a].id+'" class="ms-1" aria-label="Open website">' +
                  '<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">' +
                  '<path stroke="none" d="M0 0h24v24H0z" fill="none"/>' +
                  '<path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5" />' +
